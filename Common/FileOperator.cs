89966e4118baa3aa80f5e60d32559cdacc09b41c
Rioway <liyangwei1@outlook.com>
2018-01-07 23:23:58 +0800

    Add files via upload

---
+++ Common/FileOperator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using System.IO;
+
+namespace Common
+{
+    public static class FileOperator
+    {
+        public static List<Person> ReadFile(string filePath)
+        {
+            List<Person> objList = new List<Person>();
+            try
+            {
+                StreamReader sr = new StreamReader(filePath,Encoding.Default);
+                string line = sr.ReadLine();
+                while(line != null)
+                {
+                    string[] personArray = line.Split(',');
+                    objList.Add
+                    (
+                        new Person
+                        {
+                            PersonID = Convert.ToInt32(personArray[0]),
+                            PersonName = personArray[1],
+                            PersonMobile = personArray[2],
+                        }
+
+                   );
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return objList;
+        }
+    }
+}
